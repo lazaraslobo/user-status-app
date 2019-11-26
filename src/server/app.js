ad4b5262d09d259b95c0cc7d9739101e7c5e2c9f
lazaraslobo <lazaraslobo@gmail.com>
2019-11-26 23:35:09 +0530

    added server files with express

---
+++ src/server/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+const port = 8080;
+app.listen(port, () => console.log(`App running on port http://localhost:${port}`));
