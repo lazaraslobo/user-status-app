87d3e12a88be6c8311004466c002b96ea4ea94d6
Deepak Lobo <deepaklobo16@gmail.com>
2019-11-27 12:50:45 +0530

    moved maps to commn

--- src/components/common/header.common.js
+++ src/components/common/header.common.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Grid from '../common/grid.common';
-import GridOptions from '../maps/grid-layout.map';
+import GridOptions from './maps/grid-layout.map';
 
 const headerComp = (headerProps) =>{
     return(
         <Grid {...GridOptions.contRowCenterCenter} item xs={12} className="common-header">
             <Grid item {...GridOptions.contRowCenterCenter} xs={9}>
                 <h2>{headerProps.title}</h2>
-                <h1>HERE {console.log("ENV ", process.env)}</h1>
             </Grid>
             <Grid item {...GridOptions.contRowCenterCenter} xs={1}>
                 {headerProps.children}
